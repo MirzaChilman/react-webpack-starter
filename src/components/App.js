b22fbb4d1d65f87a9ce7eb2349729ff597c66b5b
aztrael <socket3d@yahoo.com>
2018-05-17 15:31:59 +0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Mencoba from './mencoba';
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>React!!!!!</h1>
+        <Mencoba />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/mencoba.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const mencoba = () => {
+  return (
+    <div>
+      <h1>mecoba pertama ku</h1>
+    </div>
+  );
+};
+export default mencoba;
